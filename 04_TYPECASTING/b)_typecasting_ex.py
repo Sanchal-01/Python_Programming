45eb00514fbf4f4ab9f79ed887dfd3d3df14a0b8
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-03-13 23:10:07 +0530

    Update b)_typecasting_ex.py
    Added an example of my own for typecasting.

--- 04_TYPECASTING/b)_typecasting_ex.py
+++ 04_TYPECASTING/b)_typecasting_ex.py
@@ -17,3 +17,14 @@ pi = 3.14
 pi_int = int(pi)
 print(pi_int)               # Output: 3
 print(type(pi_int))
+
+
+# Self created and solved example of typecasting 
+My_name= "Sanchal"
+My_age="22"
+c=int(My_age)
+print(c)
+print(type(c))
+
+# NOTE: int() → sirf numbers ko convert karta hai integer value mein, text ko nahi.Same goes with other data types i.e.
+#if suppose kisi variable mein Integer value stores hai to hum use string bana sakte hain but characters generate nahi kara sakte.
