c8cc98790bd49d382d69038096c5a9d749c58e2c
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-03-14 20:55:01 +0530

    Add input handling and addition programs.

---
+++ 05_INPUT-from-USER/a)_Input.py
@@ -0,0 +1,8 @@
+a = input("Enter a Number - ")
+print(a)
+
+b= input("Enter your Name - ")    # Assign the value of input function i.e name to variable b.
+print(b)                          # print b
+
+c=input("Enter your AGE - ")
+print(c)
---
+++ 05_INPUT-from-USER/b)_Add.py
@@ -0,0 +1,25 @@
+# Addition program created without any help by me:
+
+a= input("Enter the First Number - ")
+b= input("Enter the Second Number - ")
+c=int(a)
+d=int(b)
+e=c+d
+print("The sum of two numbers is : ")
+print(e)
+
+
+# More refined Version of it :
+a= input("Enter the first number - ")
+b = input("Enter the second Number - ")
+a=int(a)
+b=int(b)
+c= a+b
+print("The sum of two numbers is - ")
+print(c)
+
+
+# Most refines version of it :
+a= int(input("Enter the first Number - "))
+b= int(input ("Enter the second Number - "))
+print(a+b)
