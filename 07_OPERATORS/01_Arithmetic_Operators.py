e18ea869185d9b38ec64c76f95d60664f1068a5f
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-03-21 20:26:08 +0530

    Add Arithmetic Operators examples in python

---
+++ 07_OPERATORS/01_Arithmetic_Operators.py
@@ -0,0 +1,12 @@
+a = 20
+b = 11 
+
+# ARITHMETIC OPERATORS
+
+print("a + b = ", a+b)
+print("a - b = ", a-b)
+print("a * b = ", a*b)
+print("a / b = ", a/b)
+print("a % b = ", a%b)    # MODULO (%) operator returns the remainder of a division operation.
+print("a // b = ", a//b)  # FLOOR DIVISION-It divides the first number by the second and returns the largest integer less than or equal to the result, effectively discarding any fractional part. 
+print("a ** b = ", a**b)  # In arithmetic operations, ** is the power operator, raising the left number to the power of the right number.
