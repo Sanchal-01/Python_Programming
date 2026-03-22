e51b58cc5a5019623a58e3ce27e09131a24cd2fa
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-03-22 11:29:27 +0530

    Add Conditional Operators(python) concept & examples

---
+++ 07_OPERATORS/02_Conditional_Operators.py
@@ -0,0 +1,12 @@
+a = 12
+b = 10
+
+#CONDITIONAL OPERATORS
+print(a>12)
+print(a>=12)
+print(a<12)
+print(a<=12)
+print(a==10) #Is a equal to 10
+print(a>12)  #Is a greater than to 12
+print(a==12) # Is a equal to 12
+print(a!=12) #Is a not equal to 12
