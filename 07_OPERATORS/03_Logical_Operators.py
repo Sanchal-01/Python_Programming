2b537bee519883c728938456d57690840ab0e28f
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-03-23 20:26:43 +0530

    Add concept & examples for Logical Operators.

---
+++ 07_OPERATORS/03_Logical_Operators.py
@@ -0,0 +1,20 @@
+a = True
+b = False
+
+# For True both a and b should be True
+print("AND Operator ")
+print(True and True)
+print(False and True)
+print(True and False)
+print(False and False,"\n")
+
+# For True any a or b = True then result will be True.
+print("OR Operator")
+print(True or True)
+print(False or True)
+print(True or False)
+print(False or False,"\n")
+
+# NOT Operator
+print("If True - ",not(True))
+print("If False - " ,not(False))
