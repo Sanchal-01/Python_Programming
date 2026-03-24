2125dc8470283e01f3186de9473a21754514c5f6
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-03-24 16:45:11 +0530

    Add Assignment Operator (Concept + examples)

---
+++ 07_OPERATORS/04_Assignment_Operator.py
@@ -0,0 +1,21 @@
+a = 10
+a += 5    # Equivalent to a = (a+5)
+print(a)
+
+a -= 5    # Equivalent to a = (a-5)
+print(a)
+
+a *= 5    # Equivalent to a = (a*5)
+print(a)
+
+a /= 5    # Equivalent to a = (a/5)
+print(a)
+
+a **= 5    # Equivalent to a = (a**5)
+print(a)
+
+a //= 5    # Equivalent to a = (a//5)
+print(a)
+
+a %= 5    # Equivalent to a = (a%5)
+print(a)
