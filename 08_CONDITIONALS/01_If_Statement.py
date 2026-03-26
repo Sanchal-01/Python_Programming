58bb542ee282b1f9c5bff6d415e4ae6331d33601
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-03-26 21:27:03 +0530

    Add If_statement (concept+example)

---
+++ 08_CONDITIONALS/01_If_Statement.py
@@ -0,0 +1,7 @@
+age = 21
+
+if (age> 12):
+    print("My name is Sanchal")
+    print ("What's your name ??")
+
+print("This is outside if statement and will be printed in any case whether if condition is satisfied or not. ")
