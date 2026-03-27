39fdeb3bfddad7a1c343d1bc7cbdcce2c41fe393
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-03-27 14:21:53 +0530

    Add If_Elif_Else statements (Concept and self example)

---
+++ 08_CONDITIONALS/03_If_Elif_Else_Statement.py
@@ -0,0 +1,11 @@
+# Scenario: India scored 198 runs in the fianls against New Zealand. New Zealand needs 199 runs to win this match.
+# Design a program which can cover all the scenarios of which team will win and which won't.
+
+score = int(input("Enter the expected score here : "))
+
+if(score>198):
+    print("India will win this match")
+elif(score == 198):
+    print("It's a draw, we shall have a super over. ")
+else:
+    print("New Zealand will win this match")
