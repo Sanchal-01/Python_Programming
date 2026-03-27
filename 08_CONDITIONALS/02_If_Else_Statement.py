caa39b0fb60548b01399169bc8472a8003ca159e
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-03-27 14:21:15 +0530

    Add If_else statement(Concept and example)

---
+++ 08_CONDITIONALS/02_If_Else_Statement.py
@@ -0,0 +1,8 @@
+Promise =int(input("If you want to make a promise type 1 otherwise 0 :  ")) 
+
+if(Promise==1):
+    print("You are Eligible for the trip")
+else:
+    print("You are Not eligible for the trip")
+
+print ("End or start of this vacation ??")
