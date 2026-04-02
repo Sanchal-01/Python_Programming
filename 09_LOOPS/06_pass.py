53f3d9b2e47489519c7d1a16a13282a76d56d6bf
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-04-02 15:23:50 +0530

    Add pass (loop control statement) concept and example

---
+++ 09_LOOPS/06_pass.py
@@ -0,0 +1,11 @@
+# pass is also a loop control statement.
+# The pass statement is a placeholder that does nothing. It is used while syntax requires a statement but no action currently needed.
+# Used because we want to write some piece of code later, so for that moment we use pass.
+
+#Example
+ 
+for i in range (5):
+    if i == 3:
+        pass   #Do nothing just get out of the loop.
+    print(i)
+    print("Hey!!,My name is Sanchal")
