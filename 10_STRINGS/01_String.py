096c5b7efb23ef73383493c71abc84fd6c7249e6
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-04-06 16:43:52 +0530

    Add string concepts

---
+++ 10_STRINGS/01_String.py
@@ -0,0 +1,10 @@
+# There are three valid ways of writing/declaring a string in python.
+
+name = print('Sanchal')         # This is a single line string.
+
+name = print("Sanchal_01")           # This is also a single line string.
+
+name = print('''Sanchal_001,
+    I like the way you shine.''')          
+# This is a multi line string, we can include multiple text/ number inside it and extend it to multiple line. Data written inside triple quotes can be declared as a comment(when not associated with a function) as well as a string(when associated with a function like print()).
+
