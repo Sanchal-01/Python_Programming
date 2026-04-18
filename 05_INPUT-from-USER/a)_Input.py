67f294c735b410a79256a7fccee502b010cb7de6
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-04-18 15:27:11 +0530

    Update Input function with new example

--- 05_INPUT-from-USER/a)_Input.py
+++ 05_INPUT-from-USER/a)_Input.py
@@ -1,8 +1,12 @@
-a = input("Enter a Number - ")
-print(a)
+# a = input("Enter a Number - ")
+# print(a)
 
-b= input("Enter your Name - ")    # Assign the value of input function i.e name to variable b.
-print(b)                          # print b
+# b= input("Enter your Name - ")    # Assign the value of input function i.e name to variable b.
+# print(b)                          # print b
 
-c=input("Enter your AGE - ")
-print(c)
+# c=input("Enter your AGE - ")
+# print(c)
+
+d =int(input("Enter your age : "))
+print("Your age is ", d,"\n")
+print(type(d))
