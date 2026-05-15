893fee23c239be426a7186e9b2045b4edfefcfe7
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-05-15 20:40:16 +0530

    Update Basics with List of special characters prohibited to be used as variable name.

--- 03_VARIABLE/a)_Basics.py
+++ 03_VARIABLE/a)_Basics.py
@@ -9,4 +9,11 @@ age = 32
 #a$$ge = 32 #Invalid bescuase a variable name cannot contain special characters.
 __age = 22 #Valid 
 _age__= 21 #Valid 
-a_d_f_g_h="Sanchal" #Valid
+a_d_f_g_h="Sanchal's Notebook" #Valid
+
+
+# LIST OF SPECIAL CHARACTERS that should not be used in variable names:
+# Punctuation Marks: Commas (,), periods (.), question marks (?), and exclamation points (!).
+# Currency Symbols: Dollar sign ($), euro sign (€), and pound sign (£).
+# Mathematical Symbols: Plus (+), minus (−), multiplication (×), and division (÷).
+# Other Symbols: Ampersand (&), at sign (@), hash/pound sign (#), and asterisk (*).
