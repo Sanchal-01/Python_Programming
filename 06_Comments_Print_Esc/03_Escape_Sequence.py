3a7ec8ec19a1edd653fbe438152312ea5a89f6d9
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-06-04 19:06:26 +0530

    Add concept of new escape sequence charaters used in python.

--- 06_Comments_Print_Esc/03_Escape_Sequence.py
+++ 06_Comments_Print_Esc/03_Escape_Sequence.py
@@ -14,3 +14,15 @@ print("Hello\nWorld!")
 
 # \t is also a escape sequence character which is used to create indentation between strings.
 print("This is a tab\tcharacter.")
+
+
+# \\ is also a escape sequence character which is used to create a backslash between string values between strings.
+print("What do you\\ want to say ")
+
+
+# \" is also a escape sequence character which is used to create a double quote on selective strings.
+print("I will have a cup of \"coffee\" ")
+
+
+# \' is also a escape sequence character which is used to create a single quote on selective strings.
+print("I will have a cup of \'tea\' ")
