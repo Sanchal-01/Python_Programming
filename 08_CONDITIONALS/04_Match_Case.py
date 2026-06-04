5393bb7f8b1e5202598e634d9efe6542ab2edacb
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-06-04 19:16:29 +0530

    Add the while loop and kept the match case code block inside to keep the code running.

--- 08_CONDITIONALS/04_Match_Case.py
+++ 08_CONDITIONALS/04_Match_Case.py
@@ -1,15 +1,17 @@
-L_Number =int(input("Enter your Lottery ticket's last number : "))
+while True:
+    
+    L_Number =int(input("Enter your Lottery ticket's last number : "))
 
-# Match case matches every I/P of user with eash case values, if satisfied/fulfilled --> the defined condition of that case is executed.
-match L_Number:
-    case 2:
-        print("Better luck next time :)")
+    # Match case matches every I/P of user with eash case values, if satisfied/fulfilled --> the defined condition of that case is executed.
+    match L_Number:
+        case 2:
+            print("Better luck next time :)")
 
-    case 5:
-        print("Congrats you won a BMW car. Enjoy your trip.")
+        case 5:
+            print("Congrats you won a BMW car. Enjoy your trip.")
 
-    case 4:
-        print("Congratulations you won $3 million ")
+        case 4:
+            print("Congratulations you won $3 million ")
 
-    case _:   # Used as a default case when rest conditions are false/ unsatisfied.
-        print ("No worries : Have a peaceful life ahead.")
+        case _:   # Used as a default case when rest conditions are false/ unsatisfied.
+            print ("No worries : Have a peaceful life ahead.")
