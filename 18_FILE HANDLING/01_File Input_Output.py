089d14b4dd98eb8a6edc2024ad19c40dbc276994
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-06-09 14:57:48 +0530

    Add what is file handling in python.

---
+++ 18_FILE HANDLING/01_File Input_Output.py
@@ -0,0 +1,12 @@
+# File vs Memory
+# File : Whenever we write bulk data that we want to store forever we always use files.
+# For example we use Hard-disk for storing games, movies and mp3 files.
+# In a very similar fashion if we persist something meaning we want to keep it forever we store that thing as a file.
+
+# If we want to keep something temporarily during the run of the program we use variables for that purpose because variables store values in RAM (Random Access Memory- Very fast but volatile).
+# If we want to save some data forever we just use files the main reason for using files is persistence of data.
+
+# In python we can perform 3 operations on FILE :
+# - Read a file 
+# - Write to a file
+# - Append a file
