ff59b24b4160cf7d297df91fa5045260ea85adc8
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-06-09 14:58:12 +0530

    Add how to read a file in pyhton.

---
+++ 18_FILE HANDLING/02_read.py
@@ -0,0 +1,34 @@
+f = open("Sanchal.txt", "r" )   # Here f is a file object = Open (File name , mode)
+
+# Here r = read & t = text file ====> "rt" = read text file. It is not necessary to write rt, just "r" would work fine as well sice "t" is default.
+# Also we can use "rb" for reading binary files as well.
+
+content = f.read()    # content here is a variable 
+
+print(content, "\n")
+
+f.close()      # close function is used to flush and close the file.
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+
+# This is a example of how we can resolve the situation when the file is missing by any chance.
+
+try:
+    file = open("Sanchal.txt", "r")  # Open in read mode
+    content = file.read()  # Read the entire file content
+    print(content, "\n")
+    file.close()  # Close the file
+except FileNotFoundError:
+    print("File not found.")
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+
+# This is done when we want to perform reading line by line:
+
+try:
+    file = open("Sanchal.txt", "r")
+    for line in file: # Efficient for large files
+        print(line.strip()) # Remove newline characters
+    file.close()
+except FileNotFoundError:
+    print("File not found.")
