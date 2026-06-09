95a109bc042104b243cbdd71b68b257a209a2bd8
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-06-09 14:58:46 +0530

    Add writing to a file in python along with examples and concept

---
+++ 18_FILE HANDLING/03_write.py
@@ -0,0 +1,17 @@
+# Writing to a file : Given  the name of the file 
+
+# Whenever we commit something to a pre-existing file in write mode, the earlier content of the file would be overwritten and the new 
+# give content would completely replace the old content. 
+# That is we overrite the pre-exisiting data.
+
+
+f = open("Henry.txt" , "w")
+
+String ='''
+This side Henry, I love playong video games and cricket as well.
+I have developed multiple games. 
+I am a game developer.
+''' 
+f.write(String)
+
+f.close()
