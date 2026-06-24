525aeef8e4a4dde61b718e3eb2bf7dc305add56c
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-06-24 12:19:12 +0530

    Add information of how to use pip for package management and upgrades.

---
+++ 19_WORKING _with_EXT_LIBRARIES/02_Pip.py
@@ -0,0 +1,23 @@
+# Package Management (using pip ): 
+# pip is Python’s package installer.
+# It’s used to install, upgrade, and manage external libraries.
+
+
+# Installing a package:
+'''pip install requests'''     # Installs the "requests" library
+'''pip install numpy==1.20.0'''     # Installs a specific version
+'''pip install moviepy'''
+
+# During the installation of any package suppose "Tensorflow" we se the list of collected packages these are dependencies a package needs for proper and optimal performance and functioning.
+
+
+
+# Listing installed packages:
+'''pip list''' 
+
+# Upgrading a package:
+'''pip install --upgrade requests'''
+
+# Uninstalling a Package:
+'''pip uninstall requests'''
+
