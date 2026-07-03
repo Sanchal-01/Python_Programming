aef16276f20a2c6772d67ba53527bd65912bfcec
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-07-03 10:33:13 +0530

    feat optimization and coded readability.

--- 04_TYPECASTING/b)_typecasting_ex.py
+++ 04_TYPECASTING/b)_typecasting_ex.py
@@ -1,35 +1,32 @@
 # Convert string to integer
 num_str = "10"
 num_int = int(num_str)
-print(num_int)              # Output: 10
-print (type(num_int))
+print(num_int,"&", type(num_int))              # Output: 10
 
 
-# Convert integer to string
+
+# Converting integer to string
 num = 25
 num_str = str(num)
-print(num_str)             # Output: "25"
-print(type(num_str))
+print(num_str, "&", type(num_str))             # Output: "25"
 
 
-# Convert float to integer
+# Converting float to integer
 pi = 3.14
 pi_int = int(pi)
-print(pi_int)               # Output: 3
-print(type(pi_int))
+print(pi_int, "&", type(pi_int))               # Output: 3
 
 
 # Self created and solved example of typecasting 
 My_name= "Sanchal"
 My_age="22"
 c=int(My_age)
-print(c)
-print(type(c))
+print(c, "&", type(c) )
 
 
-# NOTE: int() → sirf numbers ko convert karta hai integer value mein, text ko nahi. 
+# NOTE: int() -------> sirf numbers ko convert karta hai integer value mein, text ko nahi. 
 # g = int("sanchal")
-# print(type(g))
+# print(g, type(g))     # This would cause an error as Invalid Literal for int()
 
 # Same goes with other data types i.e.if suppose kisi variable mein Integer value stored hai to hum use string bana 
 # sakte hain but characters generate nahi kara sakte.
