df378b44aa0707294d4575d15b67c220161eb58a
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-07-11 12:20:45 +0530

    Update code structure with try except.

--- 08_CONDITIONALS/03_If_Elif_Else_Statement.py
+++ 08_CONDITIONALS/03_If_Elif_Else_Statement.py
@@ -9,3 +9,19 @@ elif(score == 198):
     print("It's a draw, we shall have a super over. ")
 else:
     print("New Zealand will win this match")
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------------#
+
+try:
+    score = int(input("Enter the expected score here : "))
+
+    if(score>198):
+        print("India will win this match")
+    elif(score == 198):
+        print("It's a draw, we shall have a super over. ")
+    else:
+        print("New Zealand will win this match")
+
+except Exception as e:
+    print ("Input integer values only",e)
